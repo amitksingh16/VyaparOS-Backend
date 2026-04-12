30448c629042af5e7aca2b5badd80b22cba414c8
Amit Singh <singh.amitk82@gmail.com>
2026-04-12 20:01:00 +0530

    FINAL FIX: Manual Hardcoded Firebase Admin with correct JSON structure

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -2,7 +2,16 @@ const admin = require('firebase-admin');
 
 // Bhai, yahan apna Service Account JSON object paste karein
 const serviceAccount = {
-  // PASTE_YOUR_JSON_OBJECT_HERE
+    "type": "service_account",
+    "project_id": "vyaparos-prod",  // <--- Ye line honi hi chahiye
+    "private_key_id": "...",
+    "private_key": "-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n",
+    "client_email": "...",
+    "client_id": "...",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "..."
 };
 
 if (serviceAccount && serviceAccount.private_key) {
